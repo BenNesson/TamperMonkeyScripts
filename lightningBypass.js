8d86e3bd72305e5a3d9dbb8513aeef74b02ae9ae
Lake <26634853+BenNesson@users.noreply.github.com>
2024-04-10 12:28:39 -0700

    fix

--- lightningBypass.js
+++ lightningBypass.js
@@ -4,7 +4,7 @@ function createLightningBypass() {
     '$$HostElementKey$$',
     '$$ShadowedNodeKey$$'
   ];
-  let _tmpSuffix = '_tmp';
+  let tmpSuffix = '_tmp';
   const mkTmp = s => s + tmpSuffix;
 
   const swapKeys = (el, oldKey, newKey) => {
@@ -16,10 +16,13 @@ function createLightningBypass() {
   const disableSingleKey = (el, ok) => swapKeys(el, ok, mkTmp(ok));
   const enableSingleKey = (el, ok) => swapKeys(el, mkTmp(ok), ok);
 
-  const forAllKeys = (el, f) => { for (const ok of filterKeys) f(el, ok); };
+  const forAllKeys = (el, f) => { for (const ok of _filterKeys) f(el, ok); };
   const disableAllKeys = el => forAllKeys(el, disableSingleKey);
   const enableAllKeys = el => forAllKeys(el, enableSingleKey);
 
+  // apparently document.all is deprecated, but the suggested replacement is querySelectorAll,
+  // which salesforce is fucking up, which is why this fucking exists.  So who knows
+  // if this'll break someday and we'll just be fucked.
   const forAllNodes = (f) => { for (const n of document.all) f(n); };
   const disableFilterAll = () => forAllNodes(disableAllKeys);
   const enableFilterAll = () => forAllNodes(enableAllKeys);
